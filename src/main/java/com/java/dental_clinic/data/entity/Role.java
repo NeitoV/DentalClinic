70d2cd1d02f73f8d9503fc76ab308dec06d48350
NeitoV <votien31102002@gmail.com>
2024-05-14 12:33:40 +0700

    user, role entity

---
+++ src/main/java/com/java/dental_clinic/data/entity/Role.java
@@ -0,0 +1,23 @@
+package com.java.dental_clinic.data.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", columnDefinition = "NVARCHAR(25)", nullable = false, unique = true)
+    private String name;
+}
---
+++ src/main/java/com/java/dental_clinic/data/entity/User.java
@@ -0,0 +1,29 @@
+package com.java.dental_clinic.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email", columnDefinition = "NVARCHAR(250)", nullable = false, unique = true)
+    private String email;
+
+    @JsonIgnore
+    @Column(name = "password", columnDefinition = "NVARCHAR(250)", nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+}
